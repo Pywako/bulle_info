a6a68d54f2b2284eae402adf4b11831a3fd3736f
pywako <wongaliceyy@gmail.com>
2018-03-08 18:20:27 +0100

    add MainController and associated view

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return $this->render('Main/homepage.html.twig',[
+        ]);
+    }
+
+}
