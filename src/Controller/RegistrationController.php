7a07d9664354e990cd2a4f32aa724680b267a1ce
pywako <wongaliceyy@gmail.com>
2018-03-13 15:53:14 +0100

    add RegistrationController and register view

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ */
+
+namespace App\Controller;
+
+
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/inscription", name="user_registration")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $user->setSubscriptionDate(date("dd/MM/yyyy HH:ii"));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render(
+            'Registration/register.html.twig', array('form' => $form->createView())
+        );
+    }
+}
