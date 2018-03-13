70d175563998aae24cf98008669f44de27026457
pywako <wongaliceyy@gmail.com>
2018-03-13 15:51:27 +0100

    add checkbox accepted terms

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,25 +7,34 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName', TextType::class)
-            ->add('lastName', TextType::class)
+            ->add('firstName', TextType::class, array(
+                'label' => 'Prénom'))
+            ->add('lastName', TextType::class, array(
+                'label' => 'Nom'))
             ->add('email', EmailType::class)
             ->add('plainPassword', RepeatedType::class, array(
                 'type' =>PasswordType::class,
                 'first_options' => array('label' => 'Mot de passe'),
                 'second_options' => array('label' => 'Confirmation mot de passe'),
+            ))
+            ->add('termsAccepted', CheckboxType::class, array(
+                'label' => 'Accepter la charte du site',
+                'mapped' => false,
+                'constraints' => new IsTrue()
             ))
         ;
     }
