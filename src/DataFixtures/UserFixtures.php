9fd41872a5dc42d93b9542f0f4cc179ab7aeafb5
pywako <wongaliceyy@gmail.com>
2018-03-20 16:27:34 +0100

    add Userfixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ */
+
+namespace App\DataFixtures;
+
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getUserData() as [$username, $password, $email, $isActive, $subscriptionDate])
+        {
+            $user = new User();
+            $user->setUsername($username);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+            $user->setEmail($email);
+            $user->setIsActive($isActive);
+            $user->setSubscriptionDate($subscriptionDate);
+
+            $manager->persist($user);
+            $this->addReference($username, $user);
+        }
+        $manager->flush();
+    }
+
+    private function getUserData(): array
+    {
+        $now = new \dateTime();
+        return [
+            ['tim', 'kitty5!','tomy@gmail.com',true, $now, ['ROLE_USER']],
+            ['lena', 'Strangekitten','tomy@gmail.com',true, $now, ['ROLE_USER']],
+            ['boardUser', 'Weirdkitten','tomy@gmail.com',true, $now, ['ROLE_ADMIN']],
+        ];
+    }
+
+
+}
