1ea04418391d15df0e2c985fdf558b14567df53f
pywako <wongaliceyy@gmail.com>
2018-03-25 17:03:13 +0200

    add step1 publication category choice controller / formType / view

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -6,36 +6,34 @@ namespace App\Controller;
 
 
 use App\Entity\Resource;
+use App\Form\Type\CategoryType;
 use App\Form\Type\ResourceType;
+use App\Form\Type\SubjectType;
 use App\Service\PublicationManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PublicationController extends Controller
 {
     /**
-     * @Route("/publier", name="publier")
+     * @Route("/publication_step1", name="publication_step1")
      * @Security("has_role('ROLE_USER')")
      */
-    public function publishAction(Request $request, PublicationManager $publicationManager)
+    public function CategoryChoiceAction(Request $request, SessionInterface $session, PublicationManager $publicationManager)
     {
-        $resource = new Resource();
-        $form = $this->createForm(ResourceType::class, $resource);
-
+        $form = $this->createForm(CategoryType::class);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $publicationManager->prepareResourceToPublish($resource);
-            $publicationManager->toDatabase($resource);
-            $this->addFlash('success', 'Ressource Publiée !');
-
-            return $this->redirectToRoute('homepage');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categorys = $form->getData()->getTitle();
+            $publicationManager->setInSession('category', $categorys);
+            return $this->redirectToRoute('publication_step2');
         }
         return $this->render(
-            'Publication/publish.html.twig', array('form' => $form->createView())
+            'Publication/categoryChoice.html.twig', array('form' => $form->createView())
         );
     }
 
---
+++ src/Form/Type/CategoryType.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ */
+
+namespace App\Form\Type;
+
+
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', EntityType::class, array(
+                'label'         => 'Nom de la catégorie',
+                'class'         => Category::class,
+                'choice_label'  => 'title',
+                'multiple'      => true
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Category::class
+        ));
+    }
+}
