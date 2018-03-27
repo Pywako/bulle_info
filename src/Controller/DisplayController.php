4d50b05f9a5a05d16e54267854c5acbb3011a22d
pywako <wongaliceyy@gmail.com>
2018-03-27 16:36:31 +0200

    add retrive data $category and $subject to displayController showsubjectAction method

--- src/Controller/DisplayController.php
+++ src/Controller/DisplayController.php
@@ -6,34 +6,37 @@ namespace App\Controller;
 
 
 use App\Entity\Category;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DisplayController extends Controller
 {
     /**
      * @Route("/show", name="show")
-     * @Security("has_role('ROLE_USER')")
      */
-    public function ShowAction()
+    public function ShowAction(SessionInterface $session)
     {
         $categorys = $this->getDoctrine()
             ->getRepository(Category::class)
             ->findAll();
+        $session->set('categorys', $categorys);
+
         return $this->render('Display/show.html.twig', [
             'categorys' => $categorys,
         ]);
     }
 
     /**
-     * @Route("/show/", name="show_sujet")
+     * @Route("/show/{title}", name="show_subject")
      */
-    public function ShowSubjectAction()
+    public function ShowSubjectAction(Category $category, SessionInterface $session )
     {
-        $subjects = "";
+        $subjects = $category->getSubjects();
+
         return $this->render('Display/_showSubject.html.twig', [
             'subjects' => $subjects,
+            'categorys' => $session->get('categorys')
         ]);
     }
 
