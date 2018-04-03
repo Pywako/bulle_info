0d854170ab0f71305dc5432241adcda9f82cf1de
pywako <wongaliceyy@gmail.com>
2018-04-03 22:19:48 +0200

    fix error login message and change locale parameters to fr

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,7 +29,7 @@ class SecurityController extends Controller
 
         return $this->render('Security/login.html.twig', array(
             'form' => $form->createView(),
-            'error' => 'le nom d\'utilisateur ou mot de passe est incorrect, veuillez réessayer',
+            'error' => $error,
             'path' => 'login'
         ));
     }
